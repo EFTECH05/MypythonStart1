833497d23d622befe48dddbe09f3b5d5453f2432
Franklin ngangu simbi <143759747+EFTECH05@users.noreply.github.com>
2026-02-06 05:39:17 +0200

    my first day learning python
    this is my first code in python

---
+++ day1.py
@@ -0,0 +1,5 @@
+print ("welcome to the band name generator ")
+city = input("which city you group up ? \n")
+
+pet = input ("what is the name of the pet \n ")
+print("your city was " + city + "...." + "and your  pet is  " + pet ) 
