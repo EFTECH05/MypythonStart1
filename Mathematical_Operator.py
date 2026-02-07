e1d27e72483e227fad4aff06a5b3253351961325
Franklin ngangu simbi <143759747+EFTECH05@users.noreply.github.com>
2026-02-07 07:01:31 +0200

    Update Mathematical_Operator.py
    ...

--- Mathematical_Operator.py
+++ Mathematical_Operator.py
@@ -3,6 +3,6 @@ height = 1.8
 iS_winning = True
 
 print (f" your score is : {score} \n your height is {height } \n and your wining value is {iS_winning} ")
-print (6+4 /2-(1*2))69
+print (6+4 /2-(1*2))
 age = 12 
 print (" you are " +str(age )  )
