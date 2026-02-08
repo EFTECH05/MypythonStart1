ed25cf376cc9dfa1505d3239b18d1f95d75e2518
Franklin ngangu simbi <143759747+EFTECH05@users.noreply.github.com>
2026-02-08 09:46:25 +0200

    if_Statement
    if_Statement exercise

--- if_Statement.py
+++ if_Statement.py
@@ -2,10 +2,10 @@ weight = 85
 height = 1.85
 bmi = weight / (height**2)
 # my if statement 
-
 if bmi > 18.5 : 
     print("underweight")
 elif bmi >= 18.5 and bmi <25 : 
     print("normal weight")
 else : 
-    print("overweight ")
+    print("overweight")
+    
