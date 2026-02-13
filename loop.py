b02f0730818964dead3cf8b71dd0f6d1b5357670
Franklin ngangu simbi <143759747+EFTECH05@users.noreply.github.com>
2026-02-13 02:17:23 +0200

    loop
    pratice

--- loop.py
+++ loop.py
@@ -1,2 +1,10 @@
-fruits = [10 ,30 , 10 , 20, 30 ]
-total_fruit = sum(fruits)
+student = [10 ,30 , 10 , 20, 30 ]
+ 
+
+
+
+max_student = 0
+for max in student :
+    if max > max_student :
+        max_student = max
+    print(max_student)
