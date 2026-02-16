2d29b12dab7a83176d3e3863e55ed96706125a53
Franklin ngangu simbi <143759747+EFTECH05@users.noreply.github.com>
2026-02-16 08:21:21 +0200

    Random_projec
    Random_projec

--- Random_project.py
+++ Random_project.py
@@ -2,12 +2,15 @@ import random
 word_list = ["aardvark" , "baboon" , "camel"] 
 choson_word = random.choice(word_list)
 print(choson_word)
+# to do llist 
+
 
 guess = input("guess a letter : ").lower()
 print(guess)
 
- for letter in  choson_word :
+for letter in  choson_word :
      if letter == guess :
         print("right")
-    else :
-       print ()
+    
+     else :
+       print ("guess")
