3f1fb8c4ce045012b93ef5785b02aad55b51f933
Franklin ngangu simbi <143759747+EFTECH05@users.noreply.github.com>
2026-02-21 07:24:37 +0200

    dictionary
    it a data structure that stores data into key value parts

---
+++ dictionary.py
@@ -0,0 +1,7 @@
+programing__language= {
+  
+    "python" :"python is a high level programming language ",
+    123:"java is a high level programming language",
+    "C++" : "C++ is a high level programming language"
+}
+print(programing__language[123])
--- loop.py
+++ loop.py
@@ -7,4 +7,5 @@ max_student = 0
 for max in student :
     if max > max_student :
         max_student = max
-    print(max_student)
+    print(max_student) 
+      
