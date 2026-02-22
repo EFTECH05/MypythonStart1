72c41d089115f04afefc23914fbbe2a97930a5bf
Franklin ngangu simbi <143759747+EFTECH05@users.noreply.github.com>
2026-02-22 08:14:24 +0200

    nested_list_and_dictionarie
    is a dictionary inside another dictionary.

---
+++ nested_list_and_dictionaries.py
@@ -0,0 +1,13 @@
+travel_log = {
+    "france": {
+        "cities_visited": ["Paris", "Lille", "Dijon"],
+        "total_visits": 12
+    },
+    "germany": {
+        "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+        "total_visits": 5
+    }
+}
+print("you have visited : ")
+print(travel_log["france"]["cities_visited"][2])
+print(travel_log["germany"]["cities_visited"][2])
