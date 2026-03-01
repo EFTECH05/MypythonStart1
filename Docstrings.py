76e6b621a72fb7324ee161097ad526a09349fd67
Franklin ngangu simbi <143759747+EFTECH05@users.noreply.github.com>
2026-03-01 08:11:33 +0200

    block scope.
    In Python:
    
    Indentation does NOT create scope.
    
    Only functions (and classes) create a new local scope.
    
    A variable only exists if that line actually runs.
    
    If a block never executes, variables inside it are never created.

--- Docstrings.py
+++ Docstrings.py
@@ -1,1 +1,16 @@
-  
+enemies = ["skeleton", "zombie", "alien"]
+game_level = 3
+if game_level < 5:
+    new_enemy = enemies[0]
+
+    print(new_enemy)
+
+# def create_enemy():
+#     enemies = ["skeleton", "zombie", "alien"]
+#     game_level = 3
+
+#     if game_level < 5:
+#         new_enemy = enemies[0]
+
+# create_enemy()
+# print(new_enemy)
